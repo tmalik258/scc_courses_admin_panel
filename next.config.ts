dc7b0a129a211554f8cbb1f93b316639b126e8c4
Talha Malik <talhamalik25.tm@gmail.com>
2025-07-28 01:04:29 +0500

    fixed blah

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,18 @@
 import type { NextConfig } from "next";
-// import path from "path";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // experimental: {
-  //   serverComponentsExternalPackages: ['@prisma/client', 'prisma']
-  // },
-  // webpack: (config, { isServer }) => {
-  //   if (!isServer) {
-  //     // Ensure that all imports of 'yjs' resolve to the same instance
-  //     config.resolve.alias['yjs'] = path.resolve(__dirname, 'node_modules/yjs')
-  //   }
-  //   return config
-  // },
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client', 'prisma']
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Ensure that all imports of 'yjs' resolve to the same instance
+      config.resolve.alias['yjs'] = path.resolve(__dirname, 'node_modules/yjs')
+    }
+    return config
+  },
   images: {
     remotePatterns: [
       {
--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -50,6 +50,13 @@ export async function updateSession(request: NextRequest) {
     return NextResponse.redirect(url)
   }
 
+  if (user && (request.nextUrl.pathname.startsWith('/login') || request.nextUrl.pathname.startsWith('/signup'))) {
+    // user is logged in, redirect to the home page
+    const url = request.nextUrl.clone()
+    url.pathname = '/'
+    return NextResponse.redirect(url)
+  }
+
   // IMPORTANT: You *must* return the supabaseResponse object as it is.
   // If you're creating a new response object with NextResponse.next() make sure to:
   // 1. Pass the request in it, like so:
